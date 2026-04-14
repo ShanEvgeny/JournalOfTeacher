3de4ca955a38036160a9201beb5e3f3023d38d70
ShanEvgeny <saevgenij76@gmail.com>
2026-04-14 13:37:48 +0800

    Сделана лаб 3

--- app/src/main/java/com/example/journalofteacher/MainActivity.kt
+++ app/src/main/java/com/example/journalofteacher/MainActivity.kt
@@ -2,47 +2,67 @@ package com.example.journalofteacher
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
-import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
+    private lateinit var editTextStudent: EditText
+    private lateinit var editTextSubject: EditText
+    private lateinit var editTextMark: EditText
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var buttonAddMark: Button
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            val discipline: EditText = findViewById(R.id.discipline_title)
-//            val button: Button = findViewById(R.id.button_create_discipline)
-//            val
-//            insets
-//        }
-        val student: EditText = findViewById(R.id.student_full_name)
-        val button: Button = findViewById(R.id.button_create_student)
-        val linearLayout: LinearLayout = findViewById(R.id.linearlayout)
-        button.setOnClickListener {
-            val text = student.text.toString()
-            val textView = TextView(this)
-            textView.setTextSize(18f)
-            textView.setPadding(20, 0, 0, 0)
-            if (student.text.isNotEmpty()){
-                textView.text = text
-                linearLayout.addView(textView)
-                student.text.clear()
-                Toast.makeText(this, "Вы добавили нового студента!", Toast.LENGTH_SHORT).show()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        toolbar = findViewById(R.id.toolbar)
+        editTextStudent = findViewById(R.id.editTextStudent)
+        editTextSubject = findViewById(R.id.editTextSubject)
+        editTextMark = findViewById(R.id.editTextMark)
+        buttonAddMark = findViewById(R.id.buttonAddMark)
+        linearLayout = findViewById(R.id.linearlayout)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = getString(R.string.app_name)
+        buttonAddMark.setOnClickListener {
+            val textStudent = editTextStudent.text.toString()
+            val textSubject = editTextSubject.text.toString()
+            val textMark = editTextMark.text.toString()
+            val textViewMarkResult = TextView(this)
+            textViewMarkResult.setTextSize(18f)
+            textViewMarkResult.setPadding(20, 20, 20, 20)
+            if (editTextStudent.text.isNotEmpty() &&
+                editTextSubject.text.isNotEmpty() &&
+                editTextMark.text.isNotEmpty()
+                ){
+                textViewMarkResult.text = "Студент: $textStudent\nПредмет:$textSubject\nОценка:$textMark"
+                linearLayout.addView(textViewMarkResult)
+                editTextStudent.text.clear()
+                editTextSubject.text.clear()
+                editTextMark.text.clear()
+                Toast.makeText(this, "Вы добавили новую оценку!", Toast.LENGTH_SHORT).show()
             }
             else {
-                Toast.makeText(this, "Поле для ФИО студента пусто", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Некоторые данные не заполнены", Toast.LENGTH_SHORT).show()
             }
         }
-
     }
+
 }
