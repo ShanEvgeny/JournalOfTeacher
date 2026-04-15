ab2f8df40777c5ce099fcd5e0662f32dfe555152
ShanEvgeny <saevgenij76@gmail.com>
2026-04-15 09:34:24 +0800

    Добавлено меню, реализованное с помощью Navigation Drawer

---
+++ app/src/main/java/com/example/journalofteacher/AddMarkFragment.kt
@@ -0,0 +1,24 @@
+package com.example.journalofteacher
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class AddMarkFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_mark, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textView = view.findViewById<TextView>(R.id.textViewAddMark)
+        textView.text = "Это фрагмент для добавления оценки"
+    }
+}
--- app/src/main/java/com/example/journalofteacher/MainActivity.kt
+++ app/src/main/java/com/example/journalofteacher/MainActivity.kt
@@ -2,67 +2,45 @@ package com.example.journalofteacher
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
     private lateinit var toolbar: Toolbar
-    private lateinit var editTextStudent: EditText
-    private lateinit var editTextSubject: EditText
-    private lateinit var editTextMark: EditText
-    private lateinit var linearLayout: LinearLayout
-    private lateinit var buttonAddMark: Button
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         toolbar = findViewById(R.id.toolbar)
-        editTextStudent = findViewById(R.id.editTextStudent)
-        editTextSubject = findViewById(R.id.editTextSubject)
-        editTextMark = findViewById(R.id.editTextMark)
-        buttonAddMark = findViewById(R.id.buttonAddMark)
-        linearLayout = findViewById(R.id.linearlayout)
         setSupportActionBar(toolbar)
-        supportActionBar?.title = getString(R.string.app_name)
-        buttonAddMark.setOnClickListener {
-            val textStudent = editTextStudent.text.toString()
-            val textSubject = editTextSubject.text.toString()
-            val textMark = editTextMark.text.toString()
-            val textViewMarkResult = TextView(this)
-            textViewMarkResult.setTextSize(18f)
-            textViewMarkResult.setPadding(20, 20, 20, 20)
-            if (textStudent.isBlank() ||
-                textSubject.isBlank() ||
-                textMark.isBlank()
-                ){
-                Toast.makeText(this, "Некоторые данные не заполнены", Toast.LENGTH_SHORT).show()
-            }
-            else if (textMark.toInt() !in 2..5){
-                Toast.makeText(this, "Некорректное значение оценки", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                textViewMarkResult.text = "Студент: $textStudent\nПредмет: $textSubject\nОценка: $textMark"
-                linearLayout.addView(textViewMarkResult)
-                Toast.makeText(this, "Вы добавили новую оценку!", Toast.LENGTH_SHORT).show()
-            }
-        }
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.fragmentListMarks, R.id.fragmentAddMark),
+            drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navigationView = findViewById(R.id.navigationView)
+        navigationView.setupWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 }
